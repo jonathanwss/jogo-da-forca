71998c60f0a4715fafeac8919867bed2ff997f6e
Felipe Souza <felipe.souza@cwi.com.br>
2016-11-11 16:44:16 -0200

    feat(core): criar projeto utilizando Web API.

---
+++ Forca/Forca.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Forca.Web.Startup))]
+
+namespace Forca.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
